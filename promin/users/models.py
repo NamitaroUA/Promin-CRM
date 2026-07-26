d79cd3afdb3b1db28164613327413196d360d556
Bohdan <bohdanwork@proton.me>
2026-07-26 15:19:53 +0300

    Re-structuring the project

--- promin/users/models.py
+++ promin/users/models.py
@@ -6,10 +6,13 @@ class User(AbstractUser):
     """
     Inheriting most of functionality from AbstractUser, Django's standart user preset.
     """
+    ROLE_CHOICES = [
+        ('teacher', 'Викладач'),
+        ('student', 'Студент')
+    ]
+
     rating = models.IntegerField(default=0)
-    role = models.CharField(max_length=50, default="Student")
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="student")
 
     def __str__(self):
         return f"{self.username} ({self.role})"
-
-#class Post(models.Model):
