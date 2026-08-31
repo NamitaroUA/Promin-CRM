fe3da2269f6c4bbd26328dffc078b63038694080
NamitaroUA <bohdanwork@proton.me>
2026-08-31 11:22:49 +0300

    Add a login functionality and template to users.

--- promin/users/urls.py
+++ promin/users/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.login, name="login"),
+    path("/login_attempt", views.user_login, name="user_login")
 ]
--- promin/users/views.py
+++ promin/users/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the home index.")
+
+def login(request):
+    return render(request, "users/login.html")
+
+def user_login(request):
+    if request.method == "POST":
+        login_value = request.POST.get('login', '')
+        print(login_value)
+        return HttpResponse(f"Received: {login_value}")
+
+    return HttpResponse("Nothing happened")
